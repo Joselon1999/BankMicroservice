f94e53466f294f3ad862527652bc6214a203c9bc
Joselon <joselon1999@hotmail.com>
2020-04-08 15:34:54 -0500

    Borrar todo

--- src/main/java/everis/bootcamp/bankMicroservice/Repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package everis.bootcamp.bankMicroservice.Repository;
-
-import everis.bootcamp.bankMicroservice.Document.Bank;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BankRepository extends ReactiveMongoRepository<Bank,String> {
-}
